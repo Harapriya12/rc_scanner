2e17b4eccb118deaa1bd62ff261fdeb6dc76dd81
Harapriya12 <pradhanharapriya815@gmail.com>
2026-03-06 17:24:24 +0530

    Updated build script and generated letter

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
-set -o errexit
 
+# Update package lists
 apt-get update
+
+# Install Tesseract OCR and English language data
 apt-get install -y tesseract-ocr
 apt-get install -y tesseract-ocr-eng
 
-pip install --upgrade pip
+# Install Python dependencies from requirements.txt
 pip install -r requirements.txt
